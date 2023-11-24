1f6667d34828e7f2ba64aeffcf8120316a7cadd4
melle97 <151934176+melle97@users.noreply.github.com>
2023-11-24 15:09:40 +0100

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {BrochureListComponent} from "./components/brochure-list/brochure-list.component";
+import {BrochureDetailsComponent} from "./components/brochure-details/brochure-details.component";
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'brochures', component: BrochureListComponent},
+  {path: 'brochureDetail', component: BrochureDetailsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule{
+}
